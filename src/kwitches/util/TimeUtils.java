f70c28cdb92ff33827c69c9aa2e15da1467a078c
thrakt <thrakt@gmail.com>
2011-01-25 15:32:44 +0900

    時刻処理を修正

--- src/kwitches/util/TimeUtils.java
+++ src/kwitches/util/TimeUtils.java
@@ -1,37 +1,60 @@
 package kwitches.util;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 public class TimeUtils {
 
+    private static SimpleDateFormat sdf = new SimpleDateFormat(
+        "yyyy/MM/dd HH:mm:ss");
+
+    /**
+     * 現在時刻をUTC+9(JST)で取得
+     *
+     * @return
+     */
     public static Date getJstDate() {
         return getJstDate(new Date());
     }
-    
+
+    /**
+     * 指定したUTC+0の時刻をUTC+9(JST)に変換して取得
+     *
+     * @param date
+     *            変換する時刻(UTC+0)
+     * @return
+     */
     public static Date getJstDate(Date date) {
         if (date == null) {
             return null;
         }
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss z");
-        sdf.setTimeZone(TimeZone.getTimeZone("JST"));
-        Date jstDate;
-        try {
-            jstDate = sdf.parse(sdf.format(date));
-        } catch (ParseException e) {
-            jstDate = null;
-        }
-        return jstDate;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.HOUR_OF_DAY, 9);
+        return calendar.getTime();
     }
-    
+
+    /**
+     * 現在時刻をUTC+9(JST)の文字列で取得
+     *
+     * @return
+     */
+    public static String getJstDateString() {
+        return getJstDateString(new Date());
+    }
+
+    /**
+     * 指定したUTC+0の時刻をUTC+9(JST)の文字列に変換して取得
+     *
+     * @param date
+     *            変換する時刻(UTC+0)
+     * @return
+     */
     public static String getJstDateString(Date date) {
         if (date == null) {
             return null;
         }
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        sdf.setTimeZone(TimeZone.getTimeZone("JST"));
-        return sdf.format(date);
+        return sdf.format(getJstDate(date));
     }
 }
