92c06c6a8371e86d47f90e9dc4348a6a0eb3a938
voidy21 <y2.void+github@gmail.com>
2011-02-06 19:10:29 +0900

    アイコン画像を自動的にリサイズして保存するようにした

--- src/kwitches/service/UploadImageService.java
+++ src/kwitches/service/UploadImageService.java
@@ -8,11 +8,19 @@ import org.slim3.util.BeanUtil;
 
 import com.google.appengine.api.datastore.Blob;
 import com.google.appengine.api.datastore.Transaction;
+import com.google.appengine.api.images.Image;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.Transform;
 
 import kwitches.model.ImageModel;
 import kwitches.model.UserModel;
 
 public class UploadImageService {
+    
+    private static ImagesService imagesService = ImagesServiceFactory.getImagesService();
+    
+    public static final int THUMBNAIL_WIDTH = 96;
     
     public ImageModel create(Map<String, Object> input, UserModel userModel) {
         Transaction tx;
@@ -28,7 +36,14 @@ public class UploadImageService {
                 newData.setFilename((String)fileImage.getShortFileName());
                 newData.setFileContentType((String)fileImage.getContentType());
                 byte[] imageData = fileImage.getData();
-                newData.setFileImage(new Blob(imageData));
+                //幅96にリサイズ
+                Image oldImage = ImagesServiceFactory.makeImage(imageData);
+                double width = oldImage.getWidth();
+                double height = oldImage.getHeight();
+                double rate = height / width;
+                Transform resize = ImagesServiceFactory.makeResize(THUMBNAIL_WIDTH, (int)(THUMBNAIL_WIDTH * rate));
+                Image resizeImage = imagesService.applyTransform(resize, oldImage);
+                newData.setFileImage(new Blob(resizeImage.getImageData()));
 
                 userModel.getIconRef().setModel(newData);   
                 tx = Datastore.beginTransaction();
