3daba133455518f6bc594fbd5718010eed0b1dcc
thrakt <thrakt@gmail.com>
2011-05-13 16:16:00 +0900

    記事投稿処理を追加 [#27]

--- src/kwitches/controller/twitter/FetchController.java
+++ src/kwitches/controller/twitter/FetchController.java
@@ -1,15 +1,11 @@
 package kwitches.controller.twitter;
 
-import java.util.Collections;
-
+import kwitches.service.twitter.ApplyPostService;
 import kwitches.service.twitter.TwitterAccessService;
 import kwitches.service.twitter.TwitterRequestService;
 
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
-
-import twitter4j.ResponseList;
-import twitter4j.Status;
 
 public class FetchController extends Controller {
 
@@ -20,12 +16,7 @@ public class FetchController extends Controller {
             return redirect(new TwitterRequestService().getAuthURL());
         }
 
-        ResponseList<Status> mentions = new TwitterAccessService().getMentionsAtCounter();
-        Collections.reverse(mentions);
-
-        for(Status s:mentions){
-            // TODO sign
-        }
+        new ApplyPostService().checkTwitterMentions();
 
         return null;
     }
