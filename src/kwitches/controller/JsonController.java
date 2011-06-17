6c519a927f28db3c8b9908ae7dbe88a914c0aef5
voidy21 <y2.void+github@gmail.com>
2011-06-17 16:43:12 +0900

    検索できなくなってたのでもとにもどした

--- src/kwitches/controller/JsonController.java
+++ src/kwitches/controller/JsonController.java
@@ -2,28 +2,23 @@ package kwitches.controller;
 
 import java.io.PrintWriter;
 
+import kwitches.service.JsonService;
+
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 import org.slim3.util.RequestMap;
 
-import kwitches.service.JsonService;
-
 public class JsonController extends Controller {
-    
-    private JsonService service;
+
+    private JsonService service = new JsonService();
     
     @Override
     public Navigation run() throws Exception {
-        search(new JsonService());
-        return null;
-    }
-     
-    public void search(JsonService service) throws Exception {
-        this.service = service;
         response.setContentType("application/json");
         PrintWriter pw = response.getWriter();
         String jsonString = service.getJson(new RequestMap(request));
         pw.println(jsonString);
         pw.close();
+        return null;
     }
 }
--- src/kwitches/controller/SearchController.java
+++ src/kwitches/controller/SearchController.java
@@ -1,14 +1,24 @@
 package kwitches.controller;
 
+import java.io.PrintWriter;
+
+import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 
 import kwitches.service.SearchService;
 
-public class SearchController extends JsonController {
+public class SearchController extends Controller {
+    
+    private SearchService service = new SearchService();
     
     @Override
     public Navigation run() throws Exception {
-        search(new SearchService());
+        response.setContentType("application/json");
+        PrintWriter pw = response.getWriter();
+        String word = asString("word");
+        String jsonString = service.search(word);
+        pw.println(jsonString);
+        pw.close();
         return null;
     }
 
